318639ab540b59954a1c752e98a74276aefb49ba
Kaua <kauafs@al.insper.edu.br>
2023-11-16 19:46:55 -0300

    começo do jogo

--- game.py
+++ game.py
@@ -9,19 +9,19 @@ HEIGHT = 500
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('T-rex running')
 
-parado = pygame.image.load(os.path.join("dinossauro", "DinoStart.png"))
-correndo = [pygame.image.load(os.path.join("dinossauro", "DinoRun1.png")), pygame.image.load(os.path.join("dinossauro", "DinoRun2.png"))]
-pulando = pygame.image.load(os.path.join("dinossauro", "DinoJump.png"))
-dinobaixo = [pygame.image.load(os.path.join("dinossauro", "DinoDuck1.png")), pygame.image.load(os.path.join("dinossauro", "DinoDuck2.png"))]
+parado = pygame.image.load(os.path.join("Pygamecerto\dinossauro", "DinoStart.png"))
+correndo = [pygame.image.load(os.path.join("Pygamecerto\dinossauro", "DinoRun1.png")), pygame.image.load(os.path.join("Pygamecerto\dinossauro", "DinoRun2.png"))]
+pulando = pygame.image.load(os.path.join("Pygamecerto\dinossauro", "DinoJump.png"))
+dinobaixo = [pygame.image.load(os.path.join("Pygamecerto\dinossauro", "DinoDuck1.png")), pygame.image.load(os.path.join("Pygamecerto\dinossauro", "DinoDuck2.png"))]
 
-cactusP = [pygame.image.load(os.path.join("Cactus", "SmallCactus1.png")), pygame.image.load(os.path.join("Cactus", "SmallCactus2.png")), pygame.image.load(os.path.join("Cactus", "SmallCactus3.png"))]
-cactusG = [pygame.image.load(os.path.join("Cactus", "LargeCactus1.png")), pygame.image.load(os.path.join("Cactus", "LargeCactus2.png")), pygame.image.load(os.path.join("Cactus", "LargeCactus3.png"))]
+cactusP = [pygame.image.load(os.path.join("Pygamecerto\cactus", "SmallCactus1.png")), pygame.image.load(os.path.join("Pygamecerto\cactus", "SmallCactus2.png")), pygame.image.load(os.path.join("Pygamecerto\cactus", "SmallCactus3.png"))]
+cactusG = [pygame.image.load(os.path.join("Pygamecerto\cactus", "LargeCactus1.png")), pygame.image.load(os.path.join("Pygamecerto\cactus", "LargeCactus2.png")), pygame.image.load(os.path.join("Pygamecerto\cactus", "LargeCactus3.png"))]
 
-passaro = [pygame.image.load(os.path.join("passaro", "Bird1.png")), pygame.image.load(os.path.join("passaro", "Bird2.png"))]
+passaro = [pygame.image.load(os.path.join("Pygamecerto\passaro", "Bird1.png")), pygame.image.load(os.path.join("Pygamecerto\passaro", "Bird2.png"))]
 
-nuvens = pygame.image.load(os.path.join("nuvem", "nuvem.png"))
+nuvens = pygame.image.load(os.path.join("Pygamecerto\ceu", "nuvem.png"))
 
-cenario = pygame.image.load(os.path.join("chao", "chao.png"))
+cenario = pygame.image.load(os.path.join("Pygamecerto\chao", "chao.png"))
 
 
 game = True
